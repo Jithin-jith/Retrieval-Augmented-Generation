bffe2a031ec27756e4b1ea155bf9b0e57d36f997
jithn-jith <jithinmkp93@gmail.com>
2025-01-26 15:29:25 +0530

    memory in Agents

---
+++ 12. Memory in Agents.py
@@ -0,0 +1,42 @@
+from llama_index.core import VectorStoreIndex
+from llama_index.embeddings.cohere import CohereEmbedding
+from llama_index.llms.cohere import Cohere
+from llama_index.core import VectorStoreIndex,Settings
+from llama_index.core import Document
+from llama_index.core.readers import SimpleDirectoryReader
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core.agent import ReActAgent
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+
+cohere_api_key = os.environ['COHERE_API_KEY']
+
+Settings.llm = Cohere(model='command-r-plus',temperature=0.7)
+Settings.embed_model = CohereEmbedding(
+    model_name="embed-english-v3.0",
+    input_type="search_query",
+    cohere_api_key=cohere_api_key)
+
+loader = SimpleDirectoryReader("./Data/Canada_budget/")
+documents = loader.load_data()
+
+index = VectorStoreIndex.from_documents(documents=documents)
+query_engine = index.as_query_engine()
+
+#Now let's turn our query engine into a query engine tool
+budget_tool = QueryEngineTool.from_defaults(
+    query_engine,
+    name="canadian_budget_2023",
+    description="A RAG engine with some basic facts about the 2023 Canadian federal budget.")
+
+agent = ReActAgent.from_tools([budget_tool])
+response = agent.chat(
+    "How much exactly was allocated to a tax credit to promote investment in green technologies in the 2023 Canadian federal budget?"
+)
+print(response)
+response = agent.chat(
+    "Divide the previous answer by 2."
+)
+print(response)
